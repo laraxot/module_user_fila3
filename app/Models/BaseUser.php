f458a2e401ae503405119ea7c87b1033e2c3c9eb
marco76tv <marco.sottana@gmail.com>
2025-04-17 16:28:16 +0200

    first

---
+++ app/Models/BaseUser.php
@@ -0,0 +1,564 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models;
+
+use Filament\Models\Contracts\HasName;
+use Filament\Models\Contracts\HasTenants;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
+use Modules\User\Database\Factories\UserFactory;
+use Modules\User\Models\Traits\HasTeams;
+use Modules\Xot\Contracts\UserContract;
+use Modules\Xot\Datas\XotData;
+use Modules\Xot\Models\Traits\RelationX;
+use Spatie\Permission\Traits\HasRoles;
+
+/**
+ * Modules\User\Models\User.
+ *
+ * @template TModel of \Illuminate\Database\Eloquent\Model
+ * @template TRelatedModel of \Illuminate\Database\Eloquent\Model
+ *
+ * @property Collection<int, OauthClient> $clients
+ * @property int|null $clients_count
+ * @property Team|null $currentTeam
+ * @property Collection<int, Device> $devices
+ * @property int|null $devices_count
+ * @property string|null $full_name
+ * @property DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property int|null $notifications_count
+ * @property Collection<int, Team> $ownedTeams
+ * @property int|null $owned_teams_count
+ * @property Collection<int, Permission> $permissions
+ * @property int|null $permissions_count
+ * @property \Modules\Xot\Contracts\ProfileContract|null $profile
+ * @property Collection<int, Role> $roles
+ * @property int|null $roles_count
+ * @property Collection<int, Team> $teams
+ * @property int|null $teams_count
+ * @property Collection<int, Tenant> $tenants
+ * @property int|null $tenants_count
+ * @property Collection<int, OauthAccessToken> $tokens
+ * @property int|null $tokens_count
+ * @property string $surname
+ * @property string|null $facebook_id
+ * @property Collection<int, SocialiteUser> $socialiteUsers
+ * @property int|null $socialite_users_count
+ * @property string|null $name
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $email
+ * @property string|null $password
+ * @property string|null $lang
+ * @property string|null $current_team_id
+ * @property bool|null $is_active
+ * @property bool|null $is_otp
+ * @property \DateTime|null $password_expires_at
+ * @property \DateTime|null $email_verified_at
+ * @property string|null $remember_token
+ * @property \DateTime|null $created_at
+ * @property \DateTime|null $updated_at
+ * @property \DateTime|null $deleted_at
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property string|null $deleted_by
+ * @property string|null $profile_photo_path
+ *
+ * @method static \Modules\User\Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User permission($permissions, $without = false)
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User role($roles, $guard = null, $without = false)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCurrentTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLang($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProfilePhotoPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withoutPermission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withoutRole($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFacebookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsOtp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePasswordExpiresAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereSurname($value)
+ *
+ * @mixin \Eloquent
+ */
+abstract class BaseUser extends Authenticatable implements HasName, HasTenants, UserContract
+{
+    /* , HasAvatar, UserJetContract, ExportsPersonalData */
+    /* , HasTeamsContract */
+    use HasApiTokens;
+    use HasFactory;
+
+    // use TwoFactorAuthenticatable; //ArtMin96
+    // use CanExportPersonalData; //ArtMin96
+    use HasRoles;
+
+    // use HasProfilePhoto; //ArtMin96
+    // use HasTeams; //ArtMin96
+    use HasTeams;
+    use HasUuids;
+
+    // use Traits\HasProfilePhoto;
+    use Notifiable;
+    use RelationX;
+    use Traits\HasAuthenticationLogTrait;
+    use Traits\HasTenants;
+
+    public $incrementing = false;
+
+    /** @var string */
+    protected $connection = 'user';
+
+    /** @var string */
+    protected $primaryKey = 'id';
+
+    /** @var string */
+    protected $keyType = 'string';
+
+    /** @var list<string> */
+    protected $fillable = [
+        'id',
+        'name',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'lang',
+        'current_team_id',
+        'is_active',
+        'is_otp', // is One Time Password
+        'password_expires_at',
+    ];
+
+    /** @var list<string> */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /** @var list<string> */
+    protected $with = [
+        'roles',
+    ];
+
+    /** @var list<string> */
+    protected $appends = [
+        // 'profile_photo_url',
+    ];
+
+    public function canAccessFilament(?Panel $panel = null): bool
+    {
+        // return $this->role_id === Role::ROLE_ADMINISTRATOR;
+        return true;
+    }
+
+    public function getFilamentName(): string
+    {
+        return sprintf(
+            '%s %s %s',
+            $this->name,
+            $this->first_name,
+            $this->last_name,
+        );
+    }
+
+    public function profile(): HasOne
+    {
+        /** @var class-string<Model> */
+        $profileClass = XotData::make()->getProfileClass();
+
+        return $this->hasOne($profileClass);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // $panel->default('admin');
+        if ($panel->getId() !== 'admin') {
+            $role = $panel->getId();
+            /*
+            $xot = XotData::make();
+            if ($xot->super_admin === $this->email) {
+                $role = Role::firstOrCreate(['name' => $role]);
+                $this->assignRole($role);
+            }
+            */
+
+            return $this->hasRole($role);
+        }
+
+        return true; // str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    }
+
+    public function canAccessSocialite(): bool
+    {
+        return true;
+    }
+
+    public function detach(Model $model): void
+    {
+        // @phpstan-ignore function.alreadyNarrowedType
+        if (method_exists($this, 'teams')) {
+            // @phpstan-ignore function.alreadyNarrowedType
+            $this->teams()->detach($model);
+        }
+    }
+
+    public function attach(Model $model): void
+    {
+        // @phpstan-ignore function.alreadyNarrowedType
+        if (method_exists($this, 'teams')) {
+            // @phpstan-ignore function.alreadyNarrowedType
+            $this->teams()->attach($model);
+        }
+    }
+
+    public function treeLabel(): string
+    {
+        return strval($this->name ?? $this->email);
+    }
+
+    public function treeSons(): Collection
+    {
+        return $this->teams ?? new Collection();
+    }
+
+    public function treeSonsCount(): int
+    {
+        return $this->teams()->count();
+    }
+
+    public function user(): UserContract
+    {
+        return $this;
+    }
+
+    public function devices(): BelongsToMany
+    {
+        return $this
+            ->belongsToManyX(Device::class);
+    }
+
+    public function socialiteUsers(): HasMany
+    {
+        return $this
+            ->hasMany(SocialiteUser::class);
+    }
+
+    public function getProviderField(string $provider, string $field): string
+    {
+        $socialiteUser = $this->socialiteUsers()->firstWhere(['provider' => $provider]);
+        if ($socialiteUser == null) {
+            throw new \Exception('SocialiteUser not found');
+        }
+
+        $res = $socialiteUser->{$field};
+        if (is_string($res)) {
+            return $res;
+        }
+        dddx($socialiteUser);
+        throw new \Exception('SocialiteUser field ['.$field.'] not found');
+    }
+
+    /**
+     * Get the entity's notifications.
+     *
+     * @return MorphMany<Notification, static|$this>
+     */
+    public function notifications(): MorphMany
+    {
+        // @phpstan-ignore return.type
+        return $this->morphMany(Notification::class, 'notifiable');
+    }
+
+    /**
+     * Get the user's latest authentication log.
+     *
+     * @return MorphOne<AuthenticationLog, static>
+     */
+    public function latestAuthentication(): MorphOne
+    {
+        // @phpstan-ignore return.type
+        return $this->morphOne(AuthenticationLog::class, 'authenticatable')
+            ->latestOfMany();
+    }
+
+    public function getFullNameAttribute(?string $value): ?string
+    {
+        return $value ?? $this->first_name.' '.$this->last_name;
+    }
+
+    public function getNameAttribute(?string $value): ?string
+    {
+        if ($value !== null || $this->getKey() === null) {
+            return $value;
+        }
+        $name = Str::of((string)$this->email)->before('@')->toString();
+        $i = 1;
+        $value = $name.'-'.$i;
+        while (self::firstWhere(['name' => $value]) !== null) {
+            $i++;
+            $value = $name.'-'.$i;
+        }
+        $this->update(['name' => $value]);
+
+        return $value;
+    }
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return Factory
+     */
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
+
+    /** @return array<string, string> */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'email_verified_at' => 'datetime',
+            // 'password' => 'hashed', //Call to undefined cast [hashed] on column [password] in model [Modules\User\Models\User].
+            'is_active' => 'boolean',
+            'roles.pivot.id' => 'string',
+            // https://github.com/beitsafe/laravel-uuid-auditing
+            // ALTER TABLE model_has_role CHANGE COLUMN `id` `id` CHAR(37) NOT NULL DEFAULT uuid();
+
+            'is_otp' => 'boolean',
+            'password_expires_at' => 'datetime',
+
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+
+            'updated_by' => 'string',
+            'created_by' => 'string',
+            'deleted_by' => 'string',
+        ];
+    }
+
+    /**
+     * Check if the user has teams.
+     */
+    public function hasTeams(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Check if the user belongs to any teams.
+     */
+    public function belongsToTeams(): bool
+    {
+        return true;
+    }
+
+    public function hasRole($role, ?string $guard = null): bool
+    {
+        return parent::hasRole($role, $guard);
+    }
+
+    public function teams(): BelongsToMany
+    {
+        return $this->belongsToMany(Team::class, 'team_user');
+    }
+
+    public function belongsToManyX(string $related, ?string $table = null, ?string $foreignPivotKey = null, ?string $relatedPivotKey = null, ?string $parentKey = null, ?string $relatedKey = null, ?string $relation = null): BelongsToMany
+    {
+        return parent::belongsToMany($related, $table, $foreignPivotKey, $relatedPivotKey, $parentKey, $relatedKey, $relation);
+    }
+
+    public function personalTeam(): Team
+    {
+        return $this->ownedTeams()->first();
+    }
+
+    public function switchTeam(Team $team): bool
+    {
+        if (! $this->belongsToTeam($team)) {
+            return false;
+        }
+
+        $this->current_team_id = $team->id;
+        $this->save();
+
+        return true;
+    }
+
+    public function allTeams(): Collection
+    {
+        return $this->teams()->get();
+    }
+
+    public function belongsToTeam(Team $team): bool
+    {
+        return $this->teams()->where('team_id', $team->id)->exists();
+    }
+
+    public function ownsTeam(Team $team): bool
+    {
+        return $this->ownedTeams()->where('id', $team->id)->exists();
+    }
+
+    public function teamRole(Team $team): ?Role
+    {
+        return $this->teams()->where('team_id', $team->id)->first()?->pivot?->role;
+    }
+
+    public function teamPermissions(Team $team): array
+    {
+        return $this->teamRole($team)?->permissions->pluck('name')->toArray() ?? [];
+    }
+
+    public function hasTeamPermission(Team $team, string $permission): bool
+    {
+        return $this->ownsTeam($team) || in_array($permission, $this->teamPermissions($team));
+    }
+
+    public function hasTeamRole(Team $team, string $role): bool
+    {
+        return $this->ownsTeam($team) || $this->teamRole($team)?->name === $role;
+    }
+
+    public function canManageTeam(Team $team): bool
+    {
+        return $this->ownsTeam($team);
+    }
+
+    public function canDeleteTeam(Team $team): bool
+    {
+        return $this->ownsTeam($team);
+    }
+
+    public function canLeaveTeam(Team $team): bool
+    {
+        return $this->belongsToTeam($team) && ! $this->ownsTeam($team);
+    }
+
+    public function canRemoveTeamMember(Team $team, User $user): bool
+    {
+        return $this->ownsTeam($team) || $this->hasTeamPermission($team, 'remove team member');
+    }
+
+    public function canAddTeamMember(Team $team): bool
+    {
+        return $this->ownsTeam($team) || $this->hasTeamPermission($team, 'add team member');
+    }
+
+    public function canUpdateTeamMember(Team $team, User $user): bool
+    {
+        return $this->ownsTeam($team) || $this->hasTeamPermission($team, 'update team member');
+    }
+
+    public function canUpdateTeam(Team $team): bool
+    {
+        return $this->ownsTeam($team) || $this->hasTeamPermission($team, 'update team');
+    }
+
+    public function canViewTeam(Team $team): bool
+    {
+        return $this->belongsToTeam($team) || $this->hasTeamPermission($team, 'view team');
+    }
+
+    public function canCreateTeam(): bool
+    {
+        return $this->hasPermissionTo('create team');
+    }
+
+    public function ownedTeams(): HasMany
+    {
+        return $this->hasMany(Team::class, 'owner_id');
+    }
+
+    public function currentTeam(): BelongsTo
+    {
+        return $this->belongsTo(Team::class, 'current_team_id');
+    }
+
+    public function tenants(): BelongsToMany
+    {
+        return $this->belongsToMany(Tenant::class, 'tenant_user');
+    }
+
+    public function authentications(): MorphMany
+    {
+        return $this->morphMany(Authentication::class, 'authenticatable');
+    }
+
+    public function latestAuthentication(): MorphOne
+    {
+        return $this->morphOne(Authentication::class, 'authenticatable')->latest();
+    }
+
+    public function getFullNameAttribute(?string $value): ?string
+    {
+        return trim($this->first_name . ' ' . $this->last_name);
+    }
+
+    public function getNameAttribute(?string $value): ?string
+    {
+        return trim($this->first_name . ' ' . $this->last_name);
+    }
+
+    protected static function newFactory(): Factory
+    {
+        return UserFactory::new();
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password_expires_at' => 'datetime',
+            'is_active' => 'boolean',
+            'is_otp' => 'boolean',
+        ];
+    }
+
+    public function hasTeams(): bool
+    {
+        return true;
+    }
+
+    public function belongsToTeams(): bool
+    {
+        return true;
+    }
+}
